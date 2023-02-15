bc02e37fc1399a637e26cc353ddc37c74de89a0d
Alusch <alexdurach@gmail.com>
2023-02-16 00:42:09 +0100

    Tabelle wird angezeigt, aber noch nicht im Design von primeNG

---
+++ src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Customer, Representative } from './customer';
+
+import { Table } from 'primeng/table';
+import { PrimeNGConfig } from 'primeng/api';
+import { CustomerService } from './customer.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./appdemo.component.scss'],
+})
+export class AppComponent {
+  customers: Customer[] = [];
+
+  selectedCustomers: Customer[] = [];
+
+  representatives: Representative[] = [];
+
+  statuses: any[] = [];
+
+  loading: boolean = true;
+
+  @ViewChild('dt') table!: Table;
+
+  constructor(
+    private customerService: CustomerService,
+    private primengConfig: PrimeNGConfig
+  ) {}
+
+  ngOnInit() {
+    this.customerService.getCustomersLarge().then((customers: Customer[]) => {
+      this.customers = customers;
+      this.loading = false;
+    });
+
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'XuXue Feng', image: 'xuxuefeng.png' },
+    ];
+
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' },
+    ];
+    this.primengConfig.ripple = true;
+  }
+
+  onActivityChange(event: Event) {
+    const value = (event.target as HTMLInputElement)?.value;
+    if (value && value.trim().length) {
+      const activity = parseInt(value);
+
+      if (!isNaN(activity)) {
+        this.table.filter(activity, 'activity', 'gte');
+      }
+    }
+  }
+
+  onDateSelect(value: Date) {
+    this.table.filter(this.formatDate(value), 'date', 'equals');
+  }
+
+  formatDate(date: Date): string {
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    return `${year}-${month}-${day}`;
+  }
+
+  onRepresentativeChange(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    this.table.filter(target.value, 'representative', 'in');
+  }
+}
